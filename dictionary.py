8f742600d4abe142a277acbeaf05f52c1f991c2a
lplleu <38284033+lplleu@users.noreply.github.com>
2018-04-19 12:49:52 +0200

    Add files via upload
    first attempt at using dictionaries

---
+++ dictionary.py
@@ -0,0 +1,22 @@
+#dictionary
+#199418
+#jedenfalls
+
+#2D array where indices(keys) are named values vs numbers
+
+myDict={}
+myDict["csc1015F"]=80.5
+myDict["day"]="Friday"
+myDict["temp"]="sunny"
+
+#you can also overwrite, e.g.
+myDict["csc1015F"]=80.8
+
+print(myDict["temp"])
+
+#problem
+
+#count ho many times a word appears in text
+
+
+text1984 = "It was a bridght cold day in April"
