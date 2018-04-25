e443f757b5800dbc326a8df3aeb17b9c395e21c2
lplleu <38284033+lplleu@users.noreply.github.com>
2018-04-25 11:49:25 +0200

    Add files via upload
    hanoi

---
+++ hanoi.py
@@ -0,0 +1,13 @@
+def hanoi(n,source,spare,destination):
+  #stopping case
+  if n>0: #there is nothing to solve if n==0
+    hanoi(n-1,source,destination,spare)
+    print("move disk",n,"from",source,"to",destination)
+    hanoi(n-1,spare,source,destination)
+
+#print what to do
+hanoi(5,'a','b','c')
+
+#recursive solved this is so few lines!!!  -vs while or for loop
+
+#write recursive code to convert...
