42296e019c89fb42600e4852022dc5a8bbf2ad92
lplleu <38284033+lplleu@users.noreply.github.com>
2018-04-29 11:34:36 +0200

    Create recHourglass.py
    still trying to wrap my head around how recursion works.

---
+++ recHourglass.py
@@ -0,0 +1,21 @@
+#jedenfalls
+#29-04-2018
+#pyramid, finally.
+
+#motho = "mothowachaba"
+#print(motho[0:1])
+
+def hourglass(x):
+  if len(x)==0:
+    pass
+  elif len(x)==1:
+    print(x[0:1])
+  elif len(x)>1:
+   print(x)
+   hourglass(x[:-1])
+   print(x)
+  
+if __name__ == 'builtins':
+  hourglass("tomorrow")
+else:
+  print("represent")
