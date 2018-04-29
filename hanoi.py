af251c78096c9a6f6ca28acc06fdf1d6f066086a
lplleu <38284033+lplleu@users.noreply.github.com>
2018-04-29 11:55:18 +0200

    Update hanoi.py
    requesting clarification

--- hanoi.py
+++ hanoi.py
@@ -1,7 +1,7 @@
 def hanoi(n,source,spare,destination):
   #stopping case
   if n>0: #there is nothing to solve if n==0
-    hanoi(n-1,source,destination,spare)
+    hanoi(n-1,source,destination,spare) #290418 -what is the purpose of "spare" here??????
     print("move disk",n,"from",source,"to",destination)
     hanoi(n-1,spare,source,destination)
 
