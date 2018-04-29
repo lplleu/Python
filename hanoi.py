48492e606035204c3527e5d899e5a70f44268351
lplleu <38284033+lplleu@users.noreply.github.com>
2018-04-29 12:21:03 +0200

    Update hanoi.py
    key to hanoi

--- hanoi.py
+++ hanoi.py
@@ -3,7 +3,7 @@ def hanoi(n,source,spare,destination):
   if n>0: #there is nothing to solve if n==0
     hanoi(n-1,source,destination,spare) #290418 -what is the purpose of "spare" here??????
     print("move disk",n,"from",source,"to",destination)
-    hanoi(n-1,spare,source,destination)
+    hanoi(n-1,spare,source,destination)  # I think this is the key. this is the +reset+. then rinse and repeat.
 
 #print what to do
 hanoi(5,'a','b','c')
