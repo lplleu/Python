3dad062e12a12be7e554faf8a2f2dc8c6c7fd846
lplleu <38284033+lplleu@users.noreply.github.com>
2018-04-29 13:45:56 +0200

    Update hanoi.py

--- hanoi.py
+++ hanoi.py
@@ -1,7 +1,10 @@
+#what this really means is, "move the stack n-1 from a to b; using c as a spare"
+
 def hanoi(n,source,spare,destination):
   #stopping case
   if n>0: #there is nothing to solve if n==0
     hanoi(n-1,source,destination,spare) #290418 -what is the purpose of "spare" here??????
+    #ans to above, the function takes 3 parameters, which are then later rearranged
     print("move disk",n,"from",source,"to",destination)
     hanoi(n-1,spare,source,destination)  # I think this is the key. this is the +reset+. then rinse and repeat.
 
