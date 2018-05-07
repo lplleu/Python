e3fe3d513445bba4924e1540483f64d8b94e1d59
LPLLEU <38284033+lplleu@users.noreply.github.com>
2018-05-07 11:33:35 +0200

    Create exceptionHandling.py

---
+++ exceptionHandling.py
@@ -0,0 +1,23 @@
+#jedenfalls
+#07-May-2018
+#exception error
+
+#types of errors: IOError, NameError, 
+
+try:
+  #x=50/0
+  f=open(filename,'r')
+  lines.append(f.readline())
+  lines.append(f.readline())
+  lines.append(f.readline())
+  f.close()
+    for line in lines:
+    print(line)
+  f.close()
+  
+except IOError:
+  print("It seems that the file",filename,"does not exist.")
+except:
+  print("Some other error occurred!")
+finally:
+  print("Executing finally clause")
