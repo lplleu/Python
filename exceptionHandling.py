77f99fd6b77159a0239409b5f074e9b760f57301
LPLLEU <38284033+lplleu@users.noreply.github.com>
2018-05-08 11:13:57 +0200

    Update exceptionHandling.py

--- exceptionHandling.py
+++ exceptionHandling.py
@@ -3,20 +3,33 @@
 #exception error
 
 #types of errors: IOError, NameError, TypeError, ValueError, ZeroDivisionError, IndexError [http://docs.python.org/y3k/library/exceptions.html]
-
-try:
-  #x=50/0
-  f=open(filename,'r')
-  lines.append(f.readline())
-  lines.append(f.readline())
-  lines.append(f.readline())
-  f.close()
-    for line in lines:
-    print(line)
-  f.close()
+fileOK=False
+while not fileOK:
+  try:
+    #x=50/0
+    inputF=input("type filename")
+    
+    if inputF=="":
+      sys.exit()
+    
+    f=open(filename,'r')
+    lines.append(f.readline())
+    lines.append(f.readline())
+    lines.append(f.readline())
+    f.close()
+      for line in lines:
+      print(line)
+    f.close()
+    fileOK=True
   
-except IOError:
-  print("It seems that the file",filename,"does not exist.")
+except IOError as er:
+  print(er)
+  #print("It seems that the file",filename,"does not exist.")
+except SystemExit:
+  check=input("quit?")
+  if check=="yes":
+    print("quiting")
+    fileOK=True
 except:
   print("Some other error occurred!")
 finally: #this will happen whether or not an error occurred
